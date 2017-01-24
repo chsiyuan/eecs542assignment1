3161390660f43cd0eba6db2c40fcb99af5c9624b
rcdu <danieldu1992@gmail.com>
2017-01-23 21:57:07 -0500

    move files from ./Assignments/code to .

--- Assignmen1/code/Tinv.m
+++
@@ -1,9 +0,0 @@
-function l=Tinv(w) 
-%---------------------------------------
-% Input:
-% w, a 1*4 row vector. And each element is a integer, so you need to
-% multiply the scalar of each axis when calculating.
-%
-% Output:
-% l, a 1*4 row vector, the orignal location of a node, in the form of [x,y,scale,theta]
-%---------------------------------------
--- Assignmen1/code/startup.m
+++
@@ -1,4 +0,0 @@
-%startup file - adds necessary paths
-addpath(pwd());
-addpath(fullfile(pwd(),'utils'));
-addpath('../buffy_s5e2_original');
