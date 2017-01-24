b397fdb0041faa22d90e83dc95d52300590abc81
rcdu <danieldu1992@gmail.com>
2017-01-23 22:00:07 -0500

    add filrs

---
+++ Initialize.m
@@ -0,0 +1,14 @@
+function F=Initialize(I,node,seq,B,child)
+%----------------------------------------------
+% Input:
+% I, the orginal image
+% node: the part num: 1=torso, 2=left upper arm, 3=right upper arm, 4=left lower arm, 
+% 5=right lower arm, 6= head
+% seq: the sequence of the image in the folder. It will be used in the match
+% cost function.
+% B: a cell contains all the Bi matrix {B1,B2,B3,B4,B5,B6}
+% child: a vector containing the indexes of children of this node. If the
+% node does not have any child, it will be empty.
+%
+% Output: initialization F, a 50*50*20*10 matrix [x,y,scale,theta]
+%---------------------------------------------------------------------------------------
---
+++ trans2.m
@@ -0,0 +1,3 @@
+function k=trans2(temp,m,n,p,q)
+    k=1+(temp-1)*[1,m,m*n,m*n*p]';
+end
